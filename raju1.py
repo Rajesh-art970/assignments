ce505915cc434770687eb5a363eac30686273397
prkrgit <138566951+prkrgit@users.noreply.github.com>
2023-07-05 20:14:40 +0530

    Add files via upload

---
+++ raju1.py
@@ -0,0 +1,46 @@
+#for i in range(1,5):
+ #   for j in range(i):
+  #      print(i,end='')
+   #     print()
+        
+#right angle triangle        
+'''n=int(input("enter the number of rows:"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("*",end="")
+    print() '''           
+        
+        #triangle
+'''n=int(input("enter the number of rows:"))
+for i in range(0,n):
+    for j in range(0,n-i-1):
+        print(end=" ")
+    for j in range(0,i+1):    
+        print("*",end="")
+    print()   '''     
+    
+'''n=int(input("enter the number of rows:"))
+for i in range(0,n):
+    for j in range(0,n-i-1):
+        print(end=" ")
+    for j in range(0,2*i+1):    
+        print("*",end="")
+    print()    '''
+    
+'''    
+for i in range(1,5):
+    for j in range(1,5):
+        print(j,end="")       
+    print()    '''
+    
+   
+'''n=int(input("enter the number:"))    
+for i in range(n,0,-1):
+    for j in range(n):
+        print(i,end="")       
+    print()    '''
+
+for i in range (4):
+    for j in range(4):
+        print("*",end="")
+    print()
